c693fb07fcba0718fa10c73bb26d3bbb4f49306f
Sumit Bajpai <sbajpai@codeaurora.org>
2015-03-04 19:12:16 +0530

    BT: Don't send unhold for held when active is dropped by CHLD=1.
    When active call is dropped from DUT side, any held call will
    automatically move to active. Do not send unhold(heldCall)
    explicitly when CHLD=1 drops active call.
    
    CRs-Fixed: 803638
    Change-Id: I80375572adb476802071a506b84aa4a8a2c0d610
    
    Conflicts:
    	src/com/android/server/telecom/BluetoothPhoneService.java

--- src/com/android/server/telecom/BluetoothPhoneService.java
+++ src/com/android/server/telecom/BluetoothPhoneService.java
@@ -629,8 +629,6 @@ public final class BluetoothPhoneService extends Service {
                 callsManager.disconnectCall(activeCall);
                 if (ringingCall != null) {
                     callsManager.answerCall(ringingCall, ringingCall.getVideoState());
-                } else if (heldCall != null) {
-                    callsManager.unholdCall(heldCall);
                 }
                 return true;
             }
@@ -1398,8 +1396,6 @@ public final class BluetoothPhoneService extends Service {
                         callsManager.disconnectCall(activeCall);
                         if (ringingCall != null) {
                             callsManager.answerCall(ringingCall, 0);
-                        } else if (backgroundCall != null) {
-                            callsManager.unholdCall(backgroundCall);
                         }
                     }
                 }
