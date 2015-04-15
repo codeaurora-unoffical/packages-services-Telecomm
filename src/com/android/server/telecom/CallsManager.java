10d350c293afc653cb1947a8fb46aab3f9f094a4
Benergy Meenan Ravuri <bravuri@codeaurora.org>
2015-04-15 20:36:52 +0530

    IMS Incorrect Call State Check For Answering Call
    Participant is getting dropped from conference call when an incoming
    call is answered on top of the held conference call. This is due
    to a child call's being checked as part of the check to ensure
    that a ringing call is answered only if an active and held call
    doesn't already exist.
    
    CRs-Fixed: 816127
    Change-Id: I0001f9b2be971bb7e1f020971f4917a1488077e3

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -1952,6 +1952,9 @@ public final class CallsManager extends Call.ListenerBase {
             }
 
             for (Call call : mCalls) {
+                if (call.getParentCall() != null) {
+                    continue;
+                }
                 if ((currentState == call.getState()) &&
                         (call.getTargetPhoneAccount() != null) &&
                         (isSameIdOrSipId(call.getTargetPhoneAccount().getId(), sub))) {
